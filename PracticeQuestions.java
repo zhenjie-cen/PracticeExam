e8d5ea9c3ae3af4bb0b417ae05c446631e627f52
Jason <cen.zhenjie@hotmail.com>
2023-08-09 22:44:04 -0400

    Initial commit

---
+++ PracticeQuestions.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class PracticeQuestions {
+    public static final double EPSILON = 0.05;
+    private String ready;
+
+    public static void main(String[] args) {
+        
+    }
+}
