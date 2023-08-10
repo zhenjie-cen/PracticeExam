57d14cf2865621a4e8d3779811043a21ec98a1af
Jason <cen.zhenjie@hotmail.com>
2023-08-09 23:08:20 -0400

    Added helper functions

--- PracticeQuestions.java
+++ PracticeQuestions.java
@@ -6,5 +6,19 @@ public class PracticeQuestions {
 
     public static void main(String[] args) {
         
-    }
+     }
+
+     public static void myFunc(int num, int[] stuff) {
+        for (int i=0; i<stuff.length; i++) {
+            if (i % 2 == 0) {
+                stuff[i] = num;
+            } else {
+                num = stuff[i];
+            }
+        }
+     }
+
+     public static boolean withinEpsilonOfRangeAway(double x, double y, double range) {
+        return false;
+     }
 }
